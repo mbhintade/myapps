ee34634063d8d82ad8f4f2acc1ddc704b1ef36b3
madanazureftp <mbhintade@gmail.com>
2016-11-04 17:53:36 +0530

    Code Changed

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
-var http = require('http');
+//var http = require('http');
 
-http.createServer(function (req, res) {
-    
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world! Madan');
-    
-}).listen(process.env.PORT || 8080);
+//http.createServer(function (req, res) {
+//    
+//    res.writeHead(200, { 'Content-Type': 'text/html' });
+//    res.end('Hello, world! Madan');
+//    
+//}).listen(process.env.PORT || 8080);
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.listen((process.env.PORT || 8080));
+// Server frontpage
+app.get('/', function (req, res) {
+    res.send('This is TestBot Server');
+});
+
+// Facebook Webhook
+app.get('/webhook', function (req, res) {
+    if (req.query['hub.verify_token'] === 'testbot_verify_token') {
+        res.send(req.query['hub.challenge']);
+    } else {
+        res.send('Invalid verify token');
+    }
+});
