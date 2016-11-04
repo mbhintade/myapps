956a5b75d446bb4cb1628844c5a01b6699f71b48
madanazureftp <mbhintade@gmail.com>
2016-11-04 17:56:55 +0530

    second commit

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
-//var http = require('http');
+var http = require('http');
+var express = require('express');
+var app = express();
+
+app.listen((process.env.PORT || 8080));
+
+app.get('/', function (req, res) {
+    res.send('This is TestBot Server');
+});
 
 //http.createServer(function (req, res) {
 //    
@@ -7,24 +15,24 @@
 //    
 //}).listen(process.env.PORT || 8080);
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var request = require('request');
-var app = express();
+//var express = require('express');
+//var bodyParser = require('body-parser');
+//var request = require('request');
+//var app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-app.listen((process.env.PORT || 8080));
-// Server frontpage
-app.get('/', function (req, res) {
-    res.send('This is TestBot Server');
-});
+//app.use(bodyParser.urlencoded({extended: false}));
+//app.use(bodyParser.json());
+//app.listen((process.env.PORT || 8080));
+//// Server frontpage
+//app.get('/', function (req, res) {
+//    res.send('This is TestBot Server');
+//});
 
-// Facebook Webhook
-app.get('/webhook', function (req, res) {
-    if (req.query['hub.verify_token'] === 'testbot_verify_token') {
-        res.send(req.query['hub.challenge']);
-    } else {
-        res.send('Invalid verify token');
-    }
-});
+//// Facebook Webhook
+//app.get('/webhook', function (req, res) {
+//    if (req.query['hub.verify_token'] === 'testbot_verify_token') {
+//        res.send(req.query['hub.challenge']);
+//    } else {
+//        res.send('Invalid verify token');
+//    }
+//});
