de96cb42e0e7854bb079eb8debeda104333cad4a
madanazureftp <mbhintade@gmail.com>
2016-11-04 18:23:25 +0530

    Code Changed for webhook post

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var http = require('http');
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var request = require('request');
 
 app.listen((process.env.PORT || 8080));
 
 app.get('/', function (req, res) {
-    res.send('1360571908');
+    res.send('Hi! Madan Here.');
 });
 
 //http.createServer(function (req, res) {
@@ -36,3 +38,40 @@ app.get('/webhook', function (req, res) {
         res.send('Invalid verify token');
     }
 });
+
+// respond to post calls from facebook
+app.post('/webhook/', function (req, res) {
+  var messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    var event = req.body.entry[0].messaging[i];
+    var sender = event.sender.id;
+    if (event.message && event.message.text) {
+      var incomingText = event.message.text;
+      console.log('You sent the message', incomingText);
+      sendTextMessage(sender, "Text received, echo: "+ incomingText.substring(0, 200));
+    }
+  }
+  res.sendStatus(200);
+});
+
+function sendTextMessage(sender, text) {
+  var access_token ='EAAQo1ecZCQhUBAN6amiKZAdRnZBbyqGf6NIpKkajT2e8EAZClDMdXZAfplsM8vRP5IK3hWWumLEUUUdtnPZBZAbcpHj4HWDm0snPLZBdYpzCKFS0t02kTwzcSdzhzRwdj3PESmNZBgdKS75iCh7VokfA9rpBkdnG1KIIppQPVzeTISH2OZCNZAPgNwI';
+  var messageData = {
+    text:text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:access_token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
